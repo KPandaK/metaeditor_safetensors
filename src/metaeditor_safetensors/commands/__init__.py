3f702e4a0a5388632de65894da6ced1ab45f969b
KPandaK <7730174+KPandaK@users.noreply.github.com>
2025-09-03 10:55:05 -0600

    Remove unused command files and test scripts

--- src/metaeditor_safetensors/commands/__init__.py
+++
@@ -1,5 +0,0 @@
-from .command_base import Command
-from .browse_file import BrowseFileCommand
-from .load_metadata import LoadMetadataCommand
-from .save import SaveCommand
-from .set_thumbnail import SetThumbnailCommand
