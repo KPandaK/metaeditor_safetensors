8bdb27c02f82c843faa31f71c0de1573ac9ad951
KPandaK <KPandaK@users.noreply.github.com>
2025-09-04 11:43:10 -0600

    Reworked file structure Assets are now embedded in the resources folder

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Application Entry Point
+=======================
+
+This is the main script to run the Safetensors Metadata Editor application.
+
+Run with: python main.py
+"""
+
+from metaeditor_safetensors import main
+
+if __name__ == "__main__":
+    main()
---
+++ metaeditor_safetensors/views/__init__.py
@@ -0,0 +1,3 @@
+# Views package initialization
+# Import resources_rc to make it available for relative imports from UI files
+from .. import resources_rc
